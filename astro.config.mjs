2a4b3769f12a821b55897bb949fb335b2480f4be
juweeism <mijane.404@gmail.com>
2024-11-27 02:38:59 +0800

    status to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
+  output: 'hybrid',
   adapter: cloudflare()
 });
